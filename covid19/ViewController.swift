a6b27b95b6ae8f039a8a1f16424cf9ed495baef5
Dyllanruth <Dyllan.ruth@gmail.com>
2022-03-22 15:35:43 -0500

    Initial Commit

---
+++ covid19/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  covid19
+//
+//  Created by STUDENT-SP22 on 3/22/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
