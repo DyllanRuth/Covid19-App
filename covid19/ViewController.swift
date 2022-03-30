777c5f5fb6637d164efe4b472386b28d8c587410
Dyllanruth <Dyllan.ruth@gmail.com>
2022-03-30 07:56:33 -0500

    First push

--- covid19/ViewController.swift
+++ covid19/ViewController.swift
@@ -6,11 +6,16 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myWebView: WKWebView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        let myURL = URL(string: "https://www.arcgis.com/apps/dashboards/85320e2ea5424dfaaa75ae62e5c06e61")
+        let myRequest = URLRequest(url: myURL!)
+        myWebView.load(myRequest)
         // Do any additional setup after loading the view.
     }
 
